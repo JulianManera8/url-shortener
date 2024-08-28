ab89ccb0f2c4de11829d6f146576efbc89550cf6
JulianManera8 <julianmanera@hotmail.com>
2024-08-28 18:59:11 -0300

    created  landing page

--- src/COMPONENTS/header.jsx
+++ src/COMPONENTS/header.jsx
@@ -1,21 +1,46 @@
-import { Link, useNavigate } from 'react-router-dom'
-import { Button } from "@/components/ui/button"
-
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { LinkIcon, LogOut } from "lucide-react";
 
 export default function Header() {
+  const navigate = useNavigate();
+  const user = true;
 
-    const navigate = useNavigate()
+  return (
+    <nav className="flex justify-between items-center mt-3">
+      <Link to="/">
+        <img className="h-20" src="/logo.png" alt="Trimrr logo" />
+      </Link>
 
-    return (
-        <nav>
-            <Link to='/'> 
-                <img className='h-20' src='/logo.png' alt='Trimrr logo' />
-            </Link>
+      <div className=" px-3">
+        {!user ? (
+          <Button onClick={() => navigate("/auth")}>Button</Button>
+        ) : (
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>PA</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
 
-            <div>
-                <Button onClick={() => navigate('/auth')}>Button</Button>
-            </div>
-
-        </nav>
-    )
-}   
+            <DropdownMenuContent>
+              <DropdownMenuLabel>Julian Manera</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <LinkIcon className="mr-2"/>
+                My Links
+              </DropdownMenuItem>
+              <DropdownMenuItem className="text-red-500">
+                <LogOut className="mr-2"/>
+                LogOut
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
+      </div>
+    </nav>
+  );
+}
--- src/PAGES/LandingPage.jsx
+++ src/PAGES/LandingPage.jsx
@@ -1,7 +1,71 @@
+import { Button } from "@/COMPONENTS/ui/button";
+import { Input } from "@/COMPONENTS/ui/input";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger} from "@/components/ui/accordion"
+
+import {useState} from 'react'
+import { useNavigate } from "react-router-dom";
+
+
 export default function LandingPage() {
+    const [longUrl, setLongUrl] = useState('')
+    const navigate = useNavigate()
+    
+    const handleShorten = (e) => {
+        e.preventDefault();
+
+        if(longUrl) { 
+            navigate(`/auth?createNew=${longUrl}`)
+        }
+    }
+
     return (
-        <div>
-            LandingPage
-        </div>
-    )
+      <div className="flex flex-col items-center">
+        <h2 className="my-10 sm:my-16 text-2xl sm:text-3xl lg:text-5xl text-white text-center font-extrabold">
+          The only URL Shortener <br /> you will ever need! 👇
+        </h2>
+
+        <form 
+          className="sm:h-14 flex flex-col sm:flex-row w-full gap-2 md:px-11"
+          onSubmit={handleShorten}
+        >
+          <Input 
+          type="url" 
+          placeholder="Enter your loooong URL" 
+          value={longUrl}
+          onChange={(e) => setLongUrl(e.target.value)}
+          />
+          <Button> Make it Shorter! </Button>
+        </form>
+        <img
+          src="/banner1.jpg"
+          alt="banner"
+          className="w-full my-11 md:px-11"
+        />
+
+        <Accordion type="multiple" collapsible className=" w-full text-left">
+
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+
+          <AccordionItem value="item-2">
+            <AccordionTrigger>Do i need to create an account to use the app?</AccordionTrigger>
+            <AccordionContent>
+            Yes, Creating an account allows you to manage your URLs, view analytics, and customize your short URLs.
+            </AccordionContent>
+          </AccordionItem>
+
+          <AccordionItem value="item-3">
+            <AccordionTrigger>What analytics are available?</AccordionTrigger>
+            <AccordionContent>
+              You can view the number of clicks, geolocation data of the clicks and device types (mobile or desktop) for each of your shortened URLs.
+            </AccordionContent>
+          </AccordionItem>
+
+        </Accordion>
+      </div>
+    );
 }
