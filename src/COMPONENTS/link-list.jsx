1cd1818768fb19d3ca7f0e444b81f115e86db31f
JulianManera8 <julianmanera@hotmail.com>
2024-09-03 13:12:42 -0300

    almost finished dashboard

--- src/COMPONENTS/link-list.jsx
+++ src/COMPONENTS/link-list.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+import { Copy, Delete, Download } from "lucide-react";
 
 /* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
@@ -12,6 +14,20 @@ export default function LinkList({url, fetchUrls}) {
                 <span className="text-xs flex-1"> <b className="mr-2 text-blue-300">Original URL: </b> <span className="hover:underline"> {url?.original_url}</span>  </span>
                 <span className="text-xs"> <b className="mr-2 text-blue-300">URL created: </b> <span className="hover:underline"> {new Date(url?.created_at).toLocaleString()} </span>  </span>
             </Link>
+
+            <div className="flex gap-2">
+                <Button variant="ghost" onClick={
+                    () => { navigator.clipboard.writeText(`https://trimmr.vercel/${url?.custom_url ? url?.custom_url : url?.short_url }`)}
+                }>
+                    <Copy />
+                </Button>
+                <Button variant="ghost">
+                    <Download />
+                </Button>
+                <Button variant="ghost">
+                    <Delete />
+                </Button>
+            </div>
         </div>
     )
 }
